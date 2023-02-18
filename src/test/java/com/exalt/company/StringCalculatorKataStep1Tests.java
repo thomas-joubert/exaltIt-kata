b2e99be743170ab8ad923846613f729122dc6e4d
Thomas Joubert <thomas.joubert@epita.fr>
2023-02-18 07:03:56 +0100

    refactor: rename test file to match step
    Signed-off-by: Thomas Joubert <thomas.joubert@epita.fr>

--- src/test/java/com/exalt/company/StringCalculatorKataStep1Tests.java
+++ src/test/java/com/exalt/company/StringCalculatorKataStep1Tests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class StringCalculatorKataTests
+public class StringCalculatorKataStep1Tests
 {
     StringCalculatorKata stringCalculator = new StringCalculatorKata();
 
