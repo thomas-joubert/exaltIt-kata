f729abfd0af9417bfba64fdccec478ec94487605
Thomas Joubert <thomas.joubert@epita.fr>
2023-02-18 06:58:24 +0100

    feat(calculator): add implementation passing tests
    Signed-off-by: Thomas Joubert <thomas.joubert@epita.fr>

--- src/main/java/com/exalt/company/StringCalculatorKata.java
+++ src/main/java/com/exalt/company/StringCalculatorKata.java
@@ -1,8 +1,18 @@
 package com.exalt.company;
 
+import java.util.Arrays;
+
 public class StringCalculatorKata {
 
     int add(String numbers) {
-        throw new UnsupportedOperationException("Please, complete this method with your implementation");
+
+        if (numbers.isEmpty())
+            return 0;
+        else if (numbers.length() == 1)
+            return Integer.parseInt(numbers);
+
+        return Arrays.stream(numbers.split(","))
+                .map(Integer::parseInt)
+                .reduce(0, Integer::sum);
     }
 }
