df4f753e138a7992455dbb6d79b61d1fc4a9b0bd
Thomas Joubert <thomas.joubert@epita.fr>
2023-02-21 20:39:15 +0100

    feat(tests): add tests for step4
    Signed-off-by: Thomas Joubert <thomas.joubert@epita.fr>

---
+++ src/test/java/com/exalt/company/StringCalculatorKataStep4Tests.java
@@ -0,0 +1,43 @@
+package com.exalt.company;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class StringCalculatorKataStep4Tests {
+
+    StringCalculatorKata stringCalculator = new StringCalculatorKata();
+
+    @Test
+    public void oneNewDelimiter()
+    {
+        String operation = "//|\n1|1|1|1";
+
+        assertEquals(4, stringCalculator.add(operation));
+    }
+
+    @Test
+    public void twoNewDelimiters()
+    {
+        String operation = "//|>\n1>1|1>1";
+
+        assertEquals(4, stringCalculator.add(operation));
+    }
+
+    @Test
+    public void unregisteredDelimiter()
+    {
+        String operation = "//|\n1>1|1>1";
+
+        assertThrows(RuntimeException.class, () -> stringCalculator.add(operation));
+    }
+
+    @Test
+    public void wrongFormat()
+    {
+        String operation = "//|\n1>1||1>1";
+
+        assertThrows(RuntimeException.class, () -> stringCalculator.add(operation));
+    }
+}
